b4c6c3312e2ae0df561d9459b69129b6a1cc5018
JRGamer234 <ramosgomezjorge1@gmail.com>
2023-11-28 09:00:27 +0000

    Update funciones.js

--- 1_funcionesJS/funciones/funciones.js
+++ 1_funcionesJS/funciones/funciones.js
@@ -1,7 +1,11 @@
 let fecha = new Date();
+let edad = 18;
 function Encender() {
     alert();
 }
 function Inicio() {
     alert("La hora de conexión es "+ fecha.getHours() + ":" + fecha.getMinutes());
+}
+function miEdad(){
+    alert("Mi edad es " + edad);
 }
