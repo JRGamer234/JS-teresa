de1cc2532a111bb90681ae9b1867fd6962ef1872
JRGamer234 <ramosgomezjorge1@gmail.com>
2023-12-15 10:51:46 +0100

    1

---
+++ Examen Teresa/jorge_ramos_gomez.js
@@ -0,0 +1,27 @@
+let fecha = new Date();
+let hora = new Date().getHours();
+
+var apellido = prompt("Cual es tu apellido: ");
+var user = prompt("Cual es tu nombre?");
+function saludar() {
+    if (hora >= 8 && hora <= 15) {
+        alert("Buenos dias");
+    } else if(hora >= 21 && hora <= 7) 
+        alert("Buenas noches");
+    }
+
+function usuario(){
+
+var user = "cliente";
+
+    if ( hora == 23 && hora == 5){
+        prompt("Usuario: ");
+    }else if(user = cliente){
+    var apellido = prompt("Cual es tu apellido: ");
+    }
+}
+var apellido2 = prompt("vuelve a poner tu apellido: ")
+function mostrar() {
+    alert("Tu apellido es: " + apellido2 + " y la hora es: " + fecha.getHours() + ":" + fecha.getMinutes());
+}
+
