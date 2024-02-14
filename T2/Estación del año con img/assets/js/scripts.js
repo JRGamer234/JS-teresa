263cfb357ca09af01c185cba606eda3754004b35
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-02-14 08:19:52 +0100

    g

--- T2/Estación del año con img/assets/js/scripts.js
+++ T2/Estación del año con img/assets/js/scripts.js
@@ -1,25 +1,121 @@
+// function saberestacion() {
+
+//     // Obtener la fecha actual
+//     var fechaActual = new Date();
+
+//     // Determinar la estación del año
+//     var estacion;
+//     var dia = fechaActual.getDay();
+//     var mes = fechaActual.getMonth() + 1; // Sumar 1 para que sea del 1-12
+//     if ((mes === 3 && dia >= 20) || (mes == 6 && dia <= 20)) {
+//         estacion = "primavera";
+//     } else if ((mes === 6 && dia >= 21) || (mes == 9 && dia <= 21)) {
+//         estacion = "verano";
+//     } else if ((mes === 9 && dia >= 22) || (mes == 12 && dia <= 20)) {
+//         estacion = "otoño";
+//     } else {
+//         estacion = "invierno";
+//     }
+//     var urlImagen = "assets/img/" + estacion + ".png";
+
+//     // Poner la img
+//     var imagen = document.getElementById("imagenRecuadro");
+//     imagen.src = urlImagen;
+//     document.getElementById('estacion').textContent = `Estamos en ${estacion}`;
+// }
+
 function saberestacion() {
-
-    // Obtener la fecha actual
     var fechaActual = new Date();
-
-    // Determinar la estación del año
-    var estacion;
     var dia = fechaActual.getDay();
-    var mes = fechaActual.getMonth() + 1; // Sumar 1 para que sea del 1-12
-    if ((mes === 3 && dia >= 20) || (mes == 6 && dia <= 20)) {
-        estacion = "primavera";
-    } else if ((mes === 6 && dia >= 21) || (mes == 9 && dia <= 21)) {
-        estacion = "verano";
-    } else if ((mes === 9 && dia >= 22) || (mes == 12 && dia <= 20)) {
-        estacion = "otoño";
-    } else {
-        estacion = "invierno";
-    }
-    var urlImagen = "assets/img/" + estacion + ".png";
+    var mes = fechaActual.getMonth() + 1;
+    switch (mes) {
+        case 1:
+            {
+                alert("Invierno")
 
-    // Poner la img
-    var imagen = document.getElementById("imagenRecuadro");
-    imagen.src = urlImagen;
-    document.getElementById('estacion').textContent = `Estamos en ${estacion}`;
+                break;
+            }
+        case 2:
+            {
+                alert("Invierno")
+
+                break;
+            }
+        case 3:
+            {
+                if (dia <= 19) {
+                    alert("Invierno")
+                } else if (dia >= 20) {
+                    alert("Primavera")
+                };
+
+                break;
+            }
+        case 4:
+            {
+                alert("Primavera");
+
+                break;
+            }
+        case 5:
+            {
+                alert("Primavera");
+
+                break;
+            }
+        case 6:
+            {
+                if (dia <= 20) {
+                    alert("Primavera")
+                }
+                else if (dia >= 21) {
+                    alert("Verano")
+                };
+                break;
+            }
+        case 7:
+            {
+                alert("Verano");
+
+                break;
+            }
+        case 8:
+            {
+                alert("Verano");
+
+                break;
+            }
+        case 9:
+            {
+                if (dia <= 21) {
+                    alert("Verano")
+                }
+                else if (dia >= 22) {
+                    alert("Otoño")
+                };
+                break;
+            }
+        case 10:
+            {
+                alert("Otoño");
+
+                break;
+            }
+        case 11:
+            {
+                alert("Otoño");
+
+                break;
+            }
+        case 12:
+            {
+                if (dia <= 20) {
+                    alert("Otoño")
+                } else if (dia >= 21) {
+                    alert("Invierno")
+                };
+
+                break;
+            }
+    }
 }
